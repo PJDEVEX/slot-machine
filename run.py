b2659939195c96e11b43264836ca8d6303c5318c
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-01-30 18:55:45 +0000

    Add get betper line function

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
+
+
+# Global constants
+# Maximum number of line in the slot game
+MAX_LINES = 3
+# Maximum and minimum amout player can bet on per line
+MAX_BET = 200
+MIN_BET = 1
+
+
 def deposit():
     """
     Function to accept deposits for the game from the users
@@ -8,7 +18,7 @@ def deposit():
     3. The value entered is a digit.
     """
     while True:
-        amount = input("What would you like to deposit? $")
+        amount = input("What would you like to deposit? $ ")
         if amount.isdigit():
             amount = int(amount)
             if amount > 0:
@@ -21,4 +31,54 @@ def deposit():
     return amount
 
 
-deposit()
+def get_number_of_lines():
+    """
+    To know number of lines a player would like to bet on
+    1. it is within 1-3 (can change if needed)
+    2. ensure it is a number
+    3. continue to request untill get vaild response
+    """
+    while True:
+        lines = input(
+            f'Pleae enter the number of lines to bet on ("1-{MAX_LINES}") ? ')
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines.")
+        else:
+            print("Please enter a number.")
+
+    return lines
+
+
+def get_bet():
+    """
+    Getting bets from the player per line
+    """
+    while True:
+        amount = input("What would you like to bet on each line? $ ")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} - ${MAX_BET}.")
+        else:
+            print("Please enter a number.")
+
+    return amount
+
+
+def main():
+    """
+    Execute all the function of the programe
+    """
+    balance = deposit()
+    lines = get_number_of_lines()
+    bet = get_bet()
+    print(balance, lines, bet)
+
+
+main()
